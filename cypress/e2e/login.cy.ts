f511ee970e04fc7ecd95f8160a85bc4181e99455
Rosinaflrz <rosina.flores@iteso.mx>
2025-12-06 19:39:00 -0600

    correxion de cypress

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,25 +1,24 @@
 describe('Prueba E2E - Autenticación de Usuario', () => {
   
   beforeEach(() => {
-    cy.visit('http://localhost:5173/login') 
+    cy.visit('http://localhost:5173/signin') 
   })
 
   it('Debe mostrar error con credenciales incorrectas', () => {
-
     cy.get('input[type="email"]').type('usuario_falso@test.com')
-    
-    cy.get('input[type="password"]').type('123456')
-    
-    cy.get('button[type="submit"]').click()
-
-    cy.url().should('include', '/login')
+    cy.contains('button', 'Continue').click()
+    cy.get('input[type="password"]').should('be.visible').type('12345699')
+    cy.contains('button', 'Sign in').click()
+    cy.url().should('include', '/signin')
+    cy.contains('Invalid').should('be.visible')
   })
 
   it('Debe iniciar sesión correctamente con credenciales válidas', () => {
     cy.get('input[type="email"]').type('rosina@lighted.com')
-    cy.get('input[type="password"]').type('passwordSeguro123')
-    cy.get('button[type="submit"]').click()
-    cy.url().should('not.include', '/login') 
+    cy.contains('button', 'Continue').click()
+    cy.get('input[type="password"]').should('be.visible').type('passwordSeguro123')
+    cy.contains('button', 'Sign in').click()
+    cy.url().should('not.include', '/signin') 
   })
 
 })
--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -2,24 +2,21 @@ describe('Seguridad y Navegación', () => {
 
   it('Debe proteger la ruta /dashboard (Redirigir si no hay sesión)', () => {
     cy.visit('http://localhost:5173/dashboard')
-
-    cy.url().should('include', '/login')
-
+    cy.url().should('include', '/signin')
     cy.contains('Bienvenido').should('not.exist')
   })
 
   it('Flujo completo: Login -> Dashboard -> Logout', () => {
-    cy.visit('http://localhost:5173/login')
+    cy.visit('http://localhost:5173/signin')
+    // Login (Paso 1)
     cy.get('input[type="email"]').type('rosina@lighted.com')
-    cy.get('input[type="password"]').type('passwordSeguro123')
-    cy.get('button[type="submit"]').click()
-    
-    cy.url().should('include', '/dashboard')
-
-    cy.contains(/Cerrar|Salir|Logout|Log out/i).should('be.visible').click()
-
-    cy.url().should('include', '/login')
-    
+    cy.contains('button', 'Continue').click()
+    // Login (Paso 2)
+    cy.get('input[type="password"]').should('be.visible').type('passwordSeguro123')
+    cy.contains('button', 'Sign in').click()
+    cy.url().should('include', '/dashboard-landing')
+    cy.get('button[aria-label="log out"]').should('be.visible').click()
+    cy.url().should('include', '/signin')
   })
 
 })
